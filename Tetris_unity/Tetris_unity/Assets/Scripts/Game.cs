6829a80fdab05dfe10a6e563c4848aac47d2b488
Pahlorus <katyshev1980@yandex.ru>
2018-09-13 16:24:58 +0200

    Create project
    Signed-off-by: Pahlorus <katyshev1980@yandex.ru>

---
+++ Tetris_unity/Tetris_unity/Assets/Scripts/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    private bool[,] _glassful;
+    private int _glassfulHigh;
+    private int _glassfulWidth;
+
+
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Tetris_unity/Tetris_unity/Assets/Scripts/Tetramino.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Tetramino
+{
+    protected bool[,] _tetramino;
+    protected Color32 _color;
+    protected Vector2Int _tetraminoPos;
+
+    protected Tetramino()
+    {
+        _tetramino = new bool[4, 4];
+        _tetraminoPos.x = 0;
+        _tetraminoPos.y = 0;
+    }
+
+    protected  void Rotate()
+    {
+
+    }
+
+    protected void Move()
+    {
+
+    }
+
+}
+
+class OTetramino : Tetramino
+{
+    OTetramino()
+    { 
+        _tetramino[1, 1] = true;
+        _tetramino[1, 2] = true;
+        _tetramino[2, 1] = true;
+        _tetramino[2, 2] = true;
+    }
+
+}
+
+
