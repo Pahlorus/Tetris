1df7211e06355e4ce750f7d6d640f9aee129c940
Pahlorus <katyshev1980@yandex.ru>
2018-10-12 12:12:58 +0200

    1
    Signed-off-by: Pahlorus <katyshev1980@yandex.ru>

---
+++ Tetris_unity/Tetris_unity/Assets/Scripts/ScriptsView/TileView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GameView
+{
+    internal class TileView: MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+        internal SpriteRenderer SpriteRenderer { get { return _spriteRenderer; } }
+    }
+}
